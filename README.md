e0b51cf5ed79816f77578092ad39a1e5f5bf45f8
Rastislav <rastislav@onion.email>
2023-11-23 22:12:10 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# exchange-rounding
-Exchange rounding module
+# Exchange Rounding
+
+`exchange-rounding` is a flexible and powerful rounding utility designed for handling decimal numbers, specifically tailored for financial and exchange-related applications. It offers customizable options to suit a variety of rounding and formatting requirements.
+
+## Features
+
+- Customizable decimal places.
+- Selection of rounding methods: `round`, `floor`, `ceil`.
+- Option to keep or remove trailing zeros.
+- Ability to add prefixes and suffixes with non-breaking spaces.
+- Insertion of spaces within numbers after a specified number of decimal places.
+- Application of CSS classes to trailing zeros.
+- Locale-specific formatting options.
+
+## Installation
+
+Install the package using npm:
+
+```bash
+npm install exchange-rounding
+```
+
+Or using yarn:
+
+```bash
+yarn add exchange-rounding
+```
+
+## Usage
+
+Import `roundNumber` from the package and use it in your project:
+
+```javascript
+import roundNumber from 'exchange-rounding';
+
+const options = {
+    decimals: 2,
+    roundFunction: 'floor',
+    keepTrailingZeros: true,
+    prefix: '$',
+    suffix: 'USD',
+    spaceAfterDecimal: 3,
+    spanClass: 'trailing-zeros',
+    localeString: { style: 'currency', currency: 'USD' }
+};
+
+const rounded = roundNumber(1234.56789, options);
+console.log(rounded); // Output will depend on the options
+```
+
+## API Reference
+
+### roundNumber(number, options)
+
+Rounds a number based on the provided options.
+
+#### Parameters
+
+- `number` (Number): The number to round.
+- `options` (Object): An object containing various formatting options.
+
+#### Options
+
+- `decimals` (Number): Number of decimal places to retain (default: 8).
+- `roundFunction` (String): Method of rounding - 'round', 'floor', 'ceil' (default: 'floor').
+- `keepTrailingZeros` (Boolean): Keep or remove trailing zeros (default: true).
+- `prefix` (String): String to prepend (default: '').
+- `suffix` (String): String to append (default: '').
+- `spaceAfterDecimal` (Number | null): Insert space after specified decimals (default: null).
+- `spanClass` (String): CSS class for trailing zeros (default: '').
+- `localeString` (Intl.NumberFormatOptions | null): Locale formatting options (default: null).
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvement, feel free to open an issue on the project's [issues page](https://github.com/bchainhub/exchange-rounding/issues). If you're interested in contributing to the code, please feel free to make a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ test/roundNumber.test.ts
@@ -0,0 +1,87 @@
+import roundNumber from '../src/index';
+
+describe('roundNumber', () => {
+  it('should round a number with default parameters', () => {
+    const result = roundNumber(1234.1234567899);
+    expect(result).toBe('1234.12345678');
+  });
+});
+
+describe('roundNumber - Rounding Methods', () => {
+  it('should round up using ceil', () => {
+    const result = roundNumber(1234.12345666466, { roundFunction: 'ceil' });
+    expect(result).toBe('1234.12345667');
+  });
+
+  it('should round down using floor', () => {
+    const result = roundNumber(1234.12345677566, { roundFunction: 'floor' });
+    expect(result).toBe('1234.12345677');
+  });
+
+  it('should round to nearest using round', () => {
+    const result = roundNumber(1234.12345676466, { roundFunction: 'round' });
+    expect(result).toBe('1234.12345676');
+  });
+});
+
+describe('roundNumber - Decimal Places', () => {
+  it('should keep 2 decimal places', () => {
+    const result = roundNumber(1234.567890, { decimals: 2 });
+    expect(result).toBe('1234.56');
+  });
+
+  it('should keep no decimal places', () => {
+    const result = roundNumber(1234.567890, { decimals: 0 });
+    expect(result).toBe('1234');
+  });
+});
+
+describe('roundNumber - Trailing Zeros', () => {
+  it('should keep trailing zeros', () => {
+    const result = roundNumber(1234.5, { keepTrailingZeros: true });
+    expect(result).toBe('1234.50000000');
+  });
+
+  it('should remove trailing zeros', () => {
+    const result = roundNumber(1234.5000, { keepTrailingZeros: false });
+    expect(result).toBe('1234.5');
+  });
+});
+
+describe('roundNumber - Prefix and Suffix', () => {
+  it('should add prefix', () => {
+    const result = roundNumber(1234.56789, { prefix: '$' });
+    expect(result).toBe('$\u00A01234.56789000');
+  });
+
+  it('should add suffix', () => {
+    const result = roundNumber(1234.56789, { suffix: '€' });
+    expect(result).toBe('1234.56789000\u00A0€');
+  });
+
+  it('should add both prefix and suffix', () => {
+    const result = roundNumber(1234.56789, { prefix: '$', suffix: '€' });
+    expect(result).toBe('$\u00A01234.56789000\u00A0€');
+  });
+});
+
+describe('roundNumber - Space After Decimal', () => {
+  it('should add space after every 3 decimal places', () => {
+    const result = roundNumber(1234.56789, { spaceAfterDecimal: 3 });
+    expect(result).toBe('1234.567 890 00');
+  });
+});
+
+describe('roundNumber - Span Class for Zeros', () => {
+  it('should add span class for trailing zeros', () => {
+    const result = roundNumber(1234.5, { spanClass: 'zero-class' });
+    expect(result).toBe('1234.5<span class="zero-class">0000000</span>');
+  });
+});
+
+describe('roundNumber - Locale String Formatting', () => {
+  it('should format number with locale string', () => {
+    const result = roundNumber(1234.56789, { localeString: { style: 'currency', currency: 'USD' } });
+    expect(result).toBe('$1,234.57');
+  });
+});
