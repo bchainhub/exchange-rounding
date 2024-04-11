744a2c744e3336395724d08c61e80ab8cf02e5d3
Rastislav <rastislav@onion.email>
2024-04-11 20:02:32 +0200

    Updates
    Unlimited currency characters, aliases, unknown currency, versioning, digitalized currencies

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,15 +2,23 @@ interface RoundNumberOptions extends Intl.NumberFormatOptions {
     roundingMode?: 'ceil' | 'floor' | 'expand' | 'trunc' | 'halfCeil' | 'halfFloor' | 'halfExpand' | 'halfTrunc' | 'halfEven';
     wrapped?: boolean;
     wrappedSymbol?: string;
+    digitalized?: boolean;
+    digitalizedSymbol?: string;
+    useAliases?: boolean;
 }
 declare class ExchNumberFormat {
+    version: string;
     private formatter;
     private intlOptions;
     private customCurrencyData;
+    private aliases;
     private originalCurrency;
     constructor(locales: string | undefined, options?: RoundNumberOptions);
     format(number: number): string;
     formatToParts(number: number): Intl.NumberFormatPart[];
     private replaceCurrency;
+    private useDecimalStyle;
+    private determineLocale;
+    private addType;
 }
 export default ExchNumberFormat;
